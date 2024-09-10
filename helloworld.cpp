7e48ce862aedfc6ffbaa246fbfba7d6da12eabc5
Bryce-L2 <bryce.lombardo@mcvts.org>
2024-09-10 10:47:05 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+ 
+int main()
+{
+    int a = 2;
+    int b = 3;
+    int c = b + a;
+    float d = 1.9;
+    float e = 3.7;
+    float f = d * 3.7;
+    cout << c << endl;
+    cout << f << endl;
+    
+}
